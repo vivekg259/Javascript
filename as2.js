5ed3cafb46201a51ae167604fc889a4a051b628c
Vivek <vivekg259@outlook.com>
2023-10-22 16:06:10 +0530

    added js file

--- as2.js
+++ as2.js
@@ -1,6 +1,6 @@
 // Global, Local and Function Scope Example
 
-let x = 5; //global scope
+let x = 7; //global scope
 
 function a() {
     var x = 12 //local scope and this is also showing the example of function scope which mean the assigned value to a variable is only work in limit of function
