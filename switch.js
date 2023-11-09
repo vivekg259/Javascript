143c3b779b9d38f4f3837f5803c42a41aeecc03c
Vivek <vivekg259@outlook.com>
2023-11-09 16:14:08 +0530

    new js added

---
+++ switch.js
@@ -0,0 +1,25 @@
+switch (new Date().getDay()) {
+  case 1:
+    console.log("Today is Monday");
+    break;
+  case 2:
+    console.log("Today is Tuesday");
+    break;
+  case 3:
+    console.log("Today is Wednesday");
+    break;
+  case 4:
+    console.log("Today is Thursday");
+    break;
+  case 5:
+    console.log("Today is Friday");
+    break;
+  case 6:
+    console.log("Today is Saturday");
+    break;
+  case 7:
+    console.log("Today is Sunday");
+    break;
+  default:
+    console.log("Incorrect Input");
+}
