4aad834da725c6dd4ac933370b915a31a426c586
Vivek <vivekg259@outlook.com>
2023-11-22 14:02:50 +0530

    added new file

--- switch.js
+++ switch.js
@@ -29,7 +29,7 @@
 
 let result = marks();
 function marks() {
-    let obtMark = 49100 ;
+    let obtMark = 491000 ;
     if (obtMark>198 && obtMark<=600) {
         return (obtMark);
     } else if (obtMark<=198) {
