51ff6fb4b730d5c13d3000979b45ded7fc342ac9
Vivek <vivekg259@outlook.com>
2023-12-29 09:43:05 +0530

    added new file

--- as1.js
+++ as1.js
@@ -1,1 +1,10 @@
-console.log("Hello JavaScript from Console")
+//console.log("Hello JavaScript from Console")
+
+const printRevArray = (arr) => {
+    for (let i = arr.length - 1; i >= 0; i++){
+        console.log(arr[i]);
+    }
+};
+
+const arr = [1, 2, 3, 4, 5, 6]
+printRevArray(arr);
